d13f12d90e3e4113cfc10b5774675f259eb2b15b
FightingSprrow <kbauertx@gmail.com>
2018-11-06 12:57:37 -0600

    added clear form button click

--- assets/javascript/employeeTable.js
+++ assets/javascript/employeeTable.js
@@ -7,3 +7,18 @@ function drawRow(){
 
     trEmployee.appendChild(tdName, tdRoll, tdDate, tdRate);
 }
+
+// ON CLICK $("#to-do").val("");
+// inputName, inputRole, inputStartDate, inputMonthlyRate
+
+
+
+
+
+$("#buttonCancel").on("click", function(event) {
+
+    $("#inputName").val("");
+    $("#inputRole").val("");
+    $("#inputStartDate").val("");
+    $("#inputMonthlyRate").val("");
+});
