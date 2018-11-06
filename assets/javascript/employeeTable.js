dee3c8b1fbb723f35741c86dc9cf88b3f8b0245e
Cody Davis <Cody.1337@gmail.com>
2018-11-06 12:54:21 -0600

    created form to add employee

--- assets/javascript/employeeTable.js
+++ assets/javascript/employeeTable.js
@@ -5,18 +5,33 @@ var config = {
     projectId: "somethingawesome-c20cd",
     storageBucket: "somethingawesome-c20cd.appspot.com",
     messagingSenderId: "154210247376"
-  };
-  firebase.initializeApp(config);
+}
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  database = firebase.database();
+
+
+database.ref('employees').on("value", function(snapshot) {
+
+    // If Firebase has a highPrice and highBidder stored (first case)
+    if (snapshot.child("employee").exists()) {
+        console.log ("found Employee's...");
+
+        // Set the variables for highBidder/highPrice equal to the stored values in firebase.
+
+        console.log (snapshot);
+
+        drawRow();
+    };
+});
 
 function drawRow(){
 
-    trEmployee = document.createElement("tr");
-    tdName = document.createElement("td");
-    tdRoll = document.createElement("td");
-    tdDate = document.createElement("td");
-    tdRate = document.createElement("td");
+trEmployee = document.createElement("tr");
+tdName = document.createElement("td");
+tdRoll = document.createElement("td");
+tdDate = document.createElement("td");
+tdRate = document.createElement("td");
 
-    trEmployee.appendChild(tdName, tdRoll, tdDate, tdRate);
+trEmployee.appendChild(tdName, tdRoll, tdDate, tdRate);
 }
