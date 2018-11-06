f5254a2e1180494c47921f01308c5c8df505a0af
Cody Davis <Cody.1337@gmail.com>
2018-11-06 12:38:26 -0600

    added firebase initilization

--- assets/javascript/employeeTable.js
+++ assets/javascript/employeeTable.js
@@ -1,4 +1,17 @@
+var config = {
+    apiKey: "AIzaSyAoBn5kFWrbJNO5WMqTBktG0L37MkHFIwY",
+    authDomain: "somethingawesome-c20cd.firebaseapp.com",
+    databaseURL: "https://somethingawesome-c20cd.firebaseio.com",
+    projectId: "somethingawesome-c20cd",
+    storageBucket: "somethingawesome-c20cd.appspot.com",
+    messagingSenderId: "154210247376"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
 function drawRow(){
+
     trEmployee = document.createElement("tr");
     tdName = document.createElement("td");
     tdRoll = document.createElement("td");
